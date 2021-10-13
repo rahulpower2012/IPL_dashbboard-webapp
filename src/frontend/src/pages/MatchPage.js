20f20b98a54334b4fcdc19e13b3c083f7baa1d4e
Rahul Powar <rahulpower2012@gmail.com>
2021-10-13 12:48:55 +0530

    Added match page to display matches for a specific team

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -6,11 +6,25 @@ import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage = () => {
 
-  
+  const [matches, setMatches] = useState([]);
+
+  useEffect(
+    ()=>{
+        const fetchMatches = async ()=>{
+            const response = await fetch(`http://localhost:8080/team/Delhi Capitals/matches?year=2019`);
+            const data = await response.json();
+            console.log(data);
+            setMatches(data);
+        };
+
+        fetchMatches();
+},[]
+);
 
     return (
       <div className="MatchPage">
           <h1>Match Page</h1>
+          {matches.map(match => <MatchDetailCard teamName={`Delhi Capitals`} match = {match}/>)}
         </div>
     );
 }
